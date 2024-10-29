dcc3927fa1d4f20bf15f7de80112c38bee07459b
IshtarFolo <arbour.xavier34@gmail.com>
2024-10-29 10:39:20 -0400

    Modifications au script pour briser le pot et du prefab du pot

--- Assets/scripts/briserPot.cs
+++ Assets/scripts/briserPot.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class briserPot : MonoBehaviour
 {
-    [SerializeField] GameObject potBrise;
+    [SerializeField] GameObject potBrise; // La version brisee du pot
+    [SerializeField] GameObject plancher; // Le plancher/ sol
+
+    Rigidbody rb;
     
 
-    private void OnCollisionEnter(Collision infoCollision)
+    private void Start()
     {
-        if (infoCollision.gameObject.tag == "environnement")
+        rb = GetComponent<Rigidbody>(); // Le rigidbody
+
+        plancher = GameObject.Find("FLOOR_EffectMesh"); // Le sol
+        plancher.layer = 3; // On donne le bon layer au placnher
+    }
+
+
+    private void OnCollisionEnter(Collision infoCollision)
+    {   
+        // Detection de la velocité et de la collision avec le layer du sol ("Environnement")
+        if (this.rb.velocity.magnitude > 1 && infoCollision.gameObject.layer == 3)
         {
+            // Le pot se brise par activation de l'asset et desactivation de la version par defaut
             potBrise.SetActive(true);
             this.gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
