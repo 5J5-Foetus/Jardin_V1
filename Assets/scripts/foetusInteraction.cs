1db6e4bb1631decef4768e8b6c996aea71732a9e
Camilia El Moustarih <119824587+e0958030@users.noreply.github.com>
2024-11-26 11:07:42 -0500

    Travail sur le sdk du foetus

---
+++ Assets/scripts/foetusInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foetusInteraction : MonoBehaviour
+{
+    //Script d'interaction avec le foetus -- Il est possible de mettre le foetus dans la marmite et obtenir du compost!
+    //Déclaration des variables
+    public AudioClip foetusSonTransformation; //Son pour la transformation du foetus 
+
+    void Update()
+    {
+        //private void OnCollisionEnter(Collision infoCollision)
+        {
+            //Lorsqu'il y a collision avec la marmite, le foetus se transforme en sac de terre après 3 secondes
+            //if ((infoCollision.gameObject.CompareTag("marmite")) && (infoCollision.gameObject.name("foetus")))
+            {
+
+            }
+        }
+    }
+}
