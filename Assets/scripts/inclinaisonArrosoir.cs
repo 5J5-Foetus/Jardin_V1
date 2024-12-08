679c39e085b8fed6769717c2035859f1887d9597
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-12-07 22:57:43 -0500

    l'arrosoir est interactif, de l'eau apparait

--- Assets/scripts/inclinaisonArrosoir.cs
+++ Assets/scripts/inclinaisonArrosoir.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class inclinaisonArrosoir : MonoBehaviour
 {
-    public ParticleSystem EauArrosoir;
+    public ParticleSystem eauArrosoir;
     public float angleInclinaison = 45f; //en degrés
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform arrosoir;
+    public Vector3 bonneDirection = Vector3.forward; // Direction dans laquelle l'eau peut couler
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Calculer l'angle entre l'arrosoir et la verticale
+        float angle = Vector3.Angle(arrosoir.up, Vector3.up);
+
+        // Vérifier si l'arrosoir est orienté vers le bas
+        bool versSol = Vector3.Dot(arrosoir.forward, Vector3.up) > 0;
+
+        // Vérifier si l'entonnoir est incliné dans la bonne direction
+        bool bonneInclinaison = Vector3.Dot(arrosoir.forward, bonneDirection) > 0;
+
+        // Activer ou désactiver le système de particules selon l'inclinaison
+        if (angle > angleInclinaison && versSol && bonneInclinaison)
+        {
+            if (!eauArrosoir.isPlaying)
+                eauArrosoir.Play();
+        }
+        else
+        {
+            if (eauArrosoir.isPlaying)
+                eauArrosoir.Stop();
+        }
     }
 }
